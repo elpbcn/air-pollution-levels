b5a7ac1e79c26439819cef1958bfaefa606c44ab
Eva Lopez <evalopez84@gmail.com>
2024-06-15 18:07:29 +0200

    Adding more actions to clean the data

--- packages/data.py
+++ packages/data.py
@@ -17,6 +17,8 @@ def clean_data(df):
     '''
     A function to clean raw data:
     - Dropping unuseful columns
+    - Dropping rows with year = NA
+    - Dropping rows where pm10_concentration AND pm25_concentration AND no2_concentration are NA
     '''
 
     #Dropping columns: web_link, reference, iso3, who_ms, population_source, version, pm10_tempcov, pm25_tempcov, no2_tempcov
@@ -30,5 +32,11 @@ def clean_data(df):
                      'pm25_tempcov',
                      'no2_tempcov'],
             inplace=True)
+
+    #Dropping rows where year is NA (3 rows for India)
+    df.dropna(subset=['year'], inplace=True)
+
+    #Dropping rows where pm10_concentration AND pm25_concentration AND no2_concentration are NA
+    df.dropna(how='all', subset=['pm10_concentration', 'pm25_concentration', 'no2_concentration'], inplace=True)
 
     return df
