918820fdcfa4a23501e0f5cd18a3e07b89dc5138
Eva Lopez <evalopez84@gmail.com>
2024-06-16 17:15:38 +0200

    Folders re-structured

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(
-    name='packages',
+    name='air-pollution-levels',
     version="0.0.1",
     install_requires=requirements,
     packages=find_packages())
