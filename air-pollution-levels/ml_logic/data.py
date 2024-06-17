a1ab86c5ad5a40c861685c8a36bf6c0c0fa3b901
Dimitrios Apatzidis <dimi.apatz@gmail.com>
2024-06-17 20:05:36 +0200

    Add the class function to data.py

--- air-pollution-levels/ml_logic/data.py
+++ air-pollution-levels/ml_logic/data.py
@@ -40,3 +40,29 @@ def clean_data(df):
     df.dropna(how='all', subset=['pm10_concentration', 'pm25_concentration', 'no2_concentration'], inplace=True)
 
     return df
+
+
+def classify_concentrations(df):
+    '''
+    A function that classifies the concentrations of NO2, PM10, and PM2.5 into categories based on the European Air Quality Index (AQI) classification.
+    '''
+    # Define classification limits
+    no2_limits = [0, 40, 90, 120, 230, 340, 1000]
+    pm10_limits = [0, 10, 20, 25, 50, 75, 800]
+    pm25_limits = [0, 20, 40, 50, 100, 150, 1200]
+
+    # Classify PM10 concentrations
+    df['pm10_class'] = pd.cut(df['pm10_concentration'], bins=pm10_limits, labels=[1, 2, 3, 4, 5, 6])
+
+    # Classify PM2.5 concentrations
+    df['pm25_class'] = pd.cut(df['pm25_concentration'], bins=pm25_limits, labels=[1, 2, 3, 4, 5, 6])
+
+    # Classify NO2 concentrations
+    df['no2_class'] = pd.cut(df['no2_concentration'], bins=no2_limits, labels=[1, 2, 3, 4, 5, 6])
+
+    # Determine the target class as the maximum of the three pollutant classes
+    df['target_class'] = df[['no2_class', 'pm10_class', 'pm25_class']].max(axis=1)
+
+    # Drop the class concentration columns
+    df = df.drop(columns=['pm10_class', 'no2_class', 'pm25_class'])
+    return df
